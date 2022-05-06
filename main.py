6171d6fef52d1ee0951fdeb461a8e76887754cd5
Edgar Alejandro Mora Valdez <cruji42@gmail.com>
2022-05-05 23:11:41 -0500

    Index python

---
+++ main.py
@@ -0,0 +1,16 @@
+@app.route('/my-first-api', methods = ['GET'])
+def hello():
+    name = request.args.get('name')
+
+    if name is None:
+        text = 'Hello!'
+
+    else:
+        text = 'Hello ' + name + '!'
+
+    return jsonify({"message": text})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
