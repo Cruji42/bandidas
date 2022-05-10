bdc3502f6352885f76692d316df19ab3cdee47d4
Edgar Alejandro Mora Valdez <cruji42@gmail.com>
2022-05-09 22:51:00 -0500

    Url management

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ include_once 'utils/headers.php';
 $uri = explode("/", substr(@$_SERVER['PHP_SELF'],1));
 
 if (implode($uri) != "bandidasindex.php"){
+    echo($uri);
     $URL = rtrim($uri[2],'.php');
 }else{
     $URL = "HOME";
